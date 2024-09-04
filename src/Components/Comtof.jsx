ea4987bf25d351e5e970538d0939d83b7c2d7616
David Ngongo <ngongodavid305@gmail.com>
2024-09-04 09:24:28 -0700

    try on a good internet

--- src/Components/Comtof.jsx
+++ src/Components/Comtof.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+
 import davng from "../assets/images/davng.png";
 
 const Comtof = () => {
+  const [isOpen, setisOpen] = useState(false);
   return (
     <>
       <div className="bg-red-900 w-full h-[60vh]  flex items-center justify-center">
@@ -25,8 +27,8 @@ const Comtof = () => {
         </p> */}
       </div>
       <div>
-        <a
-          href=""
+        <button
+          onClick={() => setisOpen(!isOpen)}
           className="bg-Maroon text-white  text-1xl sm:text-1xl md:text-3xl lg:text-4xl font-bold  py-4 px-6 sm:py-3 sm:px-5 md:py-3 md:px-5 lg:py-8 lg:px-9 rounded-lg shadow-md hover:bg-black hover:text-1xl focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition duration-200 absolute z-[10] "
           style={{
             top: "50%",
@@ -34,8 +36,41 @@ const Comtof = () => {
             transform: "translate(-50%, -50%)",
           }}
         >
-          Make Appointment
-        </a>
+          {/* <a href="">Make Appointment</a> */}
+          <span> {isOpen ? "Close Appointment " : "Open  Appointment"} </span>
+
+          {isOpen ? (
+            <svg
+              className="block h-6 w-6 "
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="block h-6 w-6 "
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </button>
       </div>
     </>
   );
