8d593049b8234ea3084b8e432d51b74fe3b9b800
David Ngongo <davidngongo305@gmail.com>
2025-06-21 23:57:12 +0300

    checking path for images still

--- src/Value.js
+++ src/Value.js
@@ -3,21 +3,21 @@ import { valueHonestyIcon, valueResearchIcon } from "../public/assets";
 const values = [
   {
     id: "1",
-    icon: "../public/assets/valueicon/profe.png",
+    icon: "assets/valueicon/profe.png",
     name: "PROFESSIONALISM",
     description:
       "Anyone can say that they are professional but only values as demonstrated by behavior can prove professionalism. Professionals get their work done as expected. We pride ourselves on being professionals and this is demonstrated by the values you will find in our teams. Professionalism is simply the quality and skill expected of a professional. We are who we say we are. We are not attempting to over promise or over reach. Our promise to our clients is to be exactly who we claim to be, who you need us to be in offering our services with the commitment that’s required. That commitment will be visible to you as expressed in our supporting values.",
   },
   {
     id: "2",
-    icon: "../public/assets/valueicon/customer-centricity.png",
+    icon: "assets/valueicon/customer-centricity.png",
     name: "CLIENT-CENTRICITY",
     description:
       "At Eland we understand that we exist because the client will have challenges that need legal expertise. It’s therefore a key requirement of our team to think about the best interest of our clients in considering, addressing and designing legal solutions for clients and client businesses. Client centricity is a strategy that aims to give the client the best experiences. This is only possible if we are organized to have the client at the heart of our  firm’s  philosophy, operations, or ideas.This demands that we as a team are an organism that listens, documents, clarifies and rallies our resources around your needs and not simply around our ego or expertise. We aim to understand client needs by developing our skills for client needs assessment as members join our team. We then carry on this tradition and make sure that every client receives the benefit of this skill.",
   },
   {
     id: "3",
-    icon: "../public/assets/valueicon/Conscientiousness.png",
+    icon: "assets/valueicon/Conscientiousness.png",
     name: "CONSCIENTIOUSNESS",
     description:
       "Conscientiousness is a quality that is internal to individuals driving them to want to do the best job possible. Whereas this is a quality that can be trained, it’s mainly a quality that lives in one’s soul. At Eland we seek out brilliant advocates who demonstrate the desire to serve. Having team members who are eager to achieve for the client means that our egos and efforts are aligned to the delivery of solutions that serve the goals and needs of our clientele.Overtime, our emphasis on having a team that is brilliant yet conscientious has helped us create that individual culture as an organizational characteristic. You will feel attended to at Eland advocates even from afar because our energy will reach you. Our teams will come around you like the honey bee and you will see the herd of Elands as we serve you.",
@@ -38,7 +38,7 @@ const values = [
   },
   {
     id: "6",
-    icon: "../public/assets/valueicon/Ubuntu.png",
+    icon: "assets/valueicon/Ubuntu.png",
     name: "UBUNTU",
     description:
       "At Eland we chose being local as a value because we value Obuntu (Luganda for Ubuntu). It is impossible to have the human values of compassion and  humanity without accepting the pride of being local. Our being local means we can take our client centric professionalism and honestly care for your needs. The legal profession has been accused of being costly and often heartless. Eland advocates operates with a humanness that supports service provision because we are able to completely stand on our side of legal arguments because no challenge or need has occurred to human beings that is not human. Aliens do not live on our planet and so human beings can solve or find pathways around human challenges that arise.",
