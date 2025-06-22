9affc03d2fedd36ac7872d3861ff2bc7bd7f15da
David Ngongo <davidngongo305@gmail.com>
2025-06-22 20:48:54 +0300

    --amend

--- src/Components/Comtof.jsx
+++ src/Components/Comtof.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import davng from "../assets/images/davng.jpg";
+import davng from "assets/images/davng.jpg";
 
 import emailjs from "@emailjs/browser";
 
